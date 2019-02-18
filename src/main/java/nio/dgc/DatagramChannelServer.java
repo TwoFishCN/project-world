6c7a5f9a35b5ba00f598ac3399ba4b4a52ac1a05
何伟 <hewei@unionbigdata.com>
2019-02-18 18:28:26 +0800

    init project.

---
+++ src/main/java/nio/dgc/DatagramChannelServer.java
@@ -0,0 +1,30 @@
+package nio.dgc;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+
+public class DatagramChannelServer {
+    public static void main(String[] args) throws IOException {
+
+        DatagramChannel channel = DatagramChannel.open();
+        channel.socket().bind(new InetSocketAddress(9999));
+
+
+        while (true) {
+            ByteBuffer buf = ByteBuffer.allocate(48);
+            buf.clear();
+            channel.receive(buf);
+
+            System.out.println(buf.toString());
+
+            String value = new String(buf.array());
+
+            System.out.println("Got value : " + value);
+            if (value.contains("shutdown")) {
+                break;
+            }
+        }
+    }
+}
