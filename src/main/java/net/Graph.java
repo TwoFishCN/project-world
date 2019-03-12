0663b083ce5a80998cfdfca26d8146f938769dd7
何伟 <hewei@unionbigdata.com>
2019-03-12 15:16:18 +0800

    hao men bi.

--- src/main/java/net/Graph.java
+++ src/main/java/net/Graph.java
@@ -80,20 +80,5 @@ public class Graph {
             return "NO SUCH ROUTE";
         }
     }
-
-    public static void main(String[] args) {
-
-        /*Graph: AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7 */
-
-        String data = "AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7";
-
-        Graph graph = Graph.build(data);
-
-        System.out.println(graph.getRouteLength("A-B-C"));
-        System.out.println(graph.getRouteLength("A-D"));
-        System.out.println(graph.getRouteLength("A-D-C"));
-        System.out.println(graph.getRouteLength("A-E-B-C-D"));
-        System.out.println(graph.getRouteLength("A-E-D"));
-
-    }
+    
 }
