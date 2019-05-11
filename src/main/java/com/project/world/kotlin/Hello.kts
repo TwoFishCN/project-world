51aceaa8dccae061326b646f8fcee802d6abed79
wei.he1 <wei.he1@thoughtworks.com>
2019-05-11 23:30:45 +0800

    more base grammar of kotlin language

--- src/main/java/com/project/world/kotlin/Hello.kts
+++ src/main/java/com/project/world/kotlin/Hello.kts
@@ -42,6 +42,64 @@ fun main() {
 
 
     println("Sleep 3 send end.")
+
+    val x = 6
+    when (x) {
+        in 1..20 -> println("x include 1..20")
+        in 2..20 -> println("x include 2..20")
+        else -> println("default else.")
+    }
+
+    when {
+        x > 2 -> println("x big than 2")
+        x > 3 -> println("x big than 3")
+        else -> println("default else.")
+    }
+
+    for (id in 2..30) {
+        print(id)
+    }
+
+    fun foo() {
+        listOf(1, 2, 3, 4, 5).forEach {
+            if (it == 3) return // 非局部直接返回到 foo() 的调用者
+            print(it)
+        }
+        println("this point is unreachable")
+    }
+
+    foo()
+    println()
+
+    fun foo1() {
+        listOf(1, 2, 3, 4, 5).forEach lit@{
+            if (it == 3) return@lit // 局部返回到该 lambda 表达式的调用者，即 forEach 循环
+            print(it)
+        }
+        println(" done with explicit label")
+    }
+
+    foo1()
+
+    fun foo2() {
+        listOf(1, 2, 3, 4, 5).forEach {
+            if (it == 3) return@forEach // 局部返回到该 lambda 表达式的调用者，即 forEach 循环
+            print(it)
+        }
+        println(" done with implicit label")
+    }
+
+    foo2()
+
+    fun foo3() {
+        listOf(1, 2, 3, 4, 5).forEach(fun(value: Int) {
+            if (value == 3) return  // 局部返回到匿名函数的调用者，即 forEach 循环
+            print(value)
+        })
+        println(" done with anonymous function")
+    }
+
+    foo3()
 }
 
 main()
