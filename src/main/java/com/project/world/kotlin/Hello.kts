1ffd83b27b6d7aa66eda70a0f930c08562829c28
wei.he1 <wei.he1@thoughtworks.com>
2019-05-11 19:54:37 +0800

    more rule.

--- src/main/java/com/project/world/kotlin/Hello.kts
+++ src/main/java/com/project/world/kotlin/Hello.kts
@@ -9,16 +9,39 @@ class Hello {
     fun showB(b: String) {
         println("hello show B$b")
     }
+
+    fun showMore(vararg args: Int) {
+        val buffer = StringBuffer()
+        for (arg in args) {
+            buffer.append(arg)
+        }
+
+        println(buffer.toString())
+    }
 }
 
-println("hell world.")
-Hello().showA("niceA")
-Hello().showB("niceB")
+fun main() {
 
-val hi = HiGold.HiGold("a-value","b-value")
+    val hello = Hello()
 
-println(hi.toString())
+    println("Hell world.")
+    hello.showA("niceA")
+    hello.showB("niceB")
 
-fun main(){
+    hello.showMore(1, 2, 3, 4, 5, 6, 7)
+
+    val hi = HiGold.HiGold("a-value", "b-value")
+
+    println(hi.a + hi.b)
+
+    println(hi.toString())
+
     println("i am main.")
+
+//    Thread.sleep(3000)
+
+
+    println("Sleep 3 send end.")
 }
+
+main()
