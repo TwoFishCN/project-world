65334604a9165a99f2454759e2ea08f7ea557b04
wei.he1 <wei.he1@thoughtworks.com>
2019-05-13 09:57:49 +0800

    dynamic method for class,

--- src/main/java/com/project/world/kotlin/HiGold.kts
+++ src/main/java/com/project/world/kotlin/HiGold.kts
@@ -19,3 +19,20 @@ Constructors(1)
 fun hello(x: String) {
     println(x)
 }
+
+
+fun String.abc() {
+    println("this like ruby method.")
+}
+
+"".abc()
+
+println(arrayOf(1, 2, 3).javaClass)
+
+val abc: Array<Number> = arrayOf(1, 2, 3)
+
+println(mapOf(Pair("1", "2"))["1"])
+
+println(mapOf(Pair(1, 2)).plus(mapOf(Pair(2, 3))))
+
+println((2..33).last() == (2..33).last)
