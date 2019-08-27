1969163f73c9e39fa993cb296fe4468f6d89d424
HeWei <wei.he1@thoughtworks.com>
2019-08-27 17:54:43 +0800

    with demo.

--- src/main/java/com/project/world/lean/poi/PoiHelloWorld.java
+++ src/main/java/com/project/world/lean/poi/PoiHelloWorld.java
@@ -4,31 +4,35 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbookFactory;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.util.FileCopyUtils;
+import org.springframework.util.ResourceUtils;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class PoiHelloWorld {
 
     public static void main(String[] args) throws IOException {
-        byte[] content = Files.readAllBytes(Paths.get("Payment Report_v1.2.xlsx"));
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(content);
-        Workbook workbook = HSSFWorkbookFactory.create(inputStream);
 
-        Sheet firstSheet = workbook.createSheet("first");
-        for (int rowNum = 0; rowNum < 10000; rowNum++) {
+        Workbook workbook = HSSFWorkbookFactory.create(sourceFile(), null, true);
+
+        Sheet firstSheet = workbook.createSheet("Test11");
+        for (int rowNum = 23; rowNum < 10000; rowNum++) {
             firstSheet.createRow(rowNum).createCell(0).setCellValue(rowNum);
         }
+
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         workbook.write(outputStream);
         workbook.close();
 
-        FileCopyUtils.copy(outputStream.toByteArray(), new File("test2.xls"));
-
+        FileCopyUtils.copy(outputStream.toByteArray(), new File("test2.xlsx"));
         outputStream.close();
+
+        FileCopyUtils.copy(sourceFile(), new File("test3.xlsx"));
+    }
+
+    private static File sourceFile() throws FileNotFoundException {
+        return ResourceUtils.getFile("classpath:data/Test.xlsx");
     }
 }
