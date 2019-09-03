82f672d17e59f7b0835065ca2351d349bd1f59c3
HeWei <wei.he1@thoughtworks.com>
2019-09-03 17:54:34 +0800

    wtf shift rows have bug.

--- src/main/java/com/project/world/lean/poi/PoiHelloWorld.java
+++ src/main/java/com/project/world/lean/poi/PoiHelloWorld.java
@@ -1,26 +1,26 @@
 package com.project.world.lean.poi;
 
-import org.apache.poi.hssf.usermodel.HSSFWorkbookFactory;
+import com.google.common.io.Files;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.util.ResourceUtils;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 
 public class PoiHelloWorld {
 
     public static void main(String[] args) throws IOException {
 
-        Workbook workbook = HSSFWorkbookFactory.create(sourceFile(), null, true);
 
-        Sheet firstSheet = workbook.createSheet("Test11");
-        for (int rowNum = 23; rowNum < 10000; rowNum++) {
-            firstSheet.createRow(rowNum).createCell(0).setCellValue(rowNum);
-        }
+        Workbook workbook = WorkbookFactory.create(new ByteArrayInputStream(Files.toByteArray(sourceFile())));
+
+        Sheet firstSheet = workbook.getSheetAt(0);
+        firstSheet.shiftRows(16, firstSheet.getLastRowNum(), 10, true, true);
+//        for (int rowNum = 16; rowNum < 10; rowNum++) {
+//            firstSheet.createRow(rowNum).createCell(0).setCellValue("test" + rowNum);
+//        }
 
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         workbook.write(outputStream);
@@ -28,8 +28,6 @@ public class PoiHelloWorld {
 
         FileCopyUtils.copy(outputStream.toByteArray(), new File("test2.xlsx"));
         outputStream.close();
-
-        FileCopyUtils.copy(sourceFile(), new File("test3.xlsx"));
     }
 
     private static File sourceFile() throws FileNotFoundException {
