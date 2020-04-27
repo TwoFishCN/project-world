2e48d76d955d35af646a37701c92fe6b7299bad8
何伟 <hewei@unionbigdata.com>
2020-04-27 16:41:01 +0800

    so code.

---
+++ src/main/java/com/project/world/lean/jdk/WatchServiceDemo.java
@@ -0,0 +1,46 @@
+package com.project.world.lean.jdk;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import static java.nio.file.StandardWatchEventKinds.*;
+
+public class WatchServiceDemo {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Path path = Paths.get("D:\\nodejs");
+        WatchService watchService = FileSystems.getDefault().newWatchService();
+
+        walkAndRegisterDirectories(path, watchService);
+
+        System.out.println("Report any file changed within next 1 minutes...");
+
+        while (true) {
+            WatchKey watchKey = watchService.take();
+
+            for (WatchEvent event : watchKey.pollEvents()) {
+                Path context = (Path) event.context();
+
+                System.out.println(event.kind());
+                System.out.println(context);
+            }
+
+            watchKey.reset();
+        }
+    }
+
+    private static void walkAndRegisterDirectories(Path path, WatchService watchService) throws IOException {
+        Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                registerDirectory(dir, watchService);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
+    private static void registerDirectory(Path dir, WatchService watchService) throws IOException {
+        dir.register(watchService, ENTRY_CREATE);
+    }
+}
